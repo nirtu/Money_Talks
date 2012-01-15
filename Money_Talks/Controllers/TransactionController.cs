55da7485d5739c320f03270b9a7f1e85804a2ecf
eshakuri <eshakuri@gmail.com>
2012-01-15 14:57:37 +0200

    Added documentation to the transactions related code

--- Money_Talks/Controllers/TransactionController.cs
+++ Money_Talks/Controllers/TransactionController.cs
@@ -6,22 +6,22 @@ using System;
 
 namespace Money_Talks.Controllers
 {
+    /// <summary>
+    /// This class handles all of the user transactions operations
+    /// </summary>
     public class TransactionController : Controller
     {
+        //creating a "link" to the DBs
+
         private AccountDbContext transactionsDb = new AccountDbContext();
         private UserDbContext usersDb = new UserDbContext();
         
-        //
-        // GET: /Account/
-
         public ViewResult Tabs()
         {
             return View();
         }
 
-        //
-        // GET: /Account/
-
+        // Presenting the transactions to the sser (his own transactions)
         public ActionResult Index()
         {
             var user = from u in usersDb.UsersDB
@@ -31,7 +31,7 @@ namespace Money_Talks.Controllers
             var userModel = user.ToArray();
             ViewBag.balance = userModel[0].Balance;
 
-            //Only the appropriate user transactions
+            // Only the appropriate user transactions
             var userTransactions = from ut in transactionsDb.Transactions
                                    where ut.Username.Equals(User.Identity.Name) &&
                                    (ut.DateCreated.Month == DateTime.Now.Month)
@@ -41,6 +41,13 @@ namespace Money_Talks.Controllers
             return View(userTransactions);
         }
 
+        /// <summary>
+        /// This method provides a support to display transactions by a given date range
+        /// </summary>
+        /// <param name="startDate">The start date value</param>
+        /// <param name="endDate">The end date value</param>
+        /// <param name="submit">The submit button value</param>
+        /// <returns>The "Index" view with the relevant transactions</returns>
         [HttpPost]
         public ActionResult Index(DateTime startDate, DateTime endDate, string submit)
         {
@@ -60,26 +67,31 @@ namespace Money_Talks.Controllers
             return View(userTransactions);
         }
 
-        //
-        // GET: /Account/Details/5
-
+        /// <summary>
+        /// Allows to display transaction details
+        /// </summary>
+        /// <param name="id">The transaction ID in the database</param>
+        /// <returns>The "Details" view</returns>
         public ViewResult Details(int id)
         {
             TransactionModel transaction = transactionsDb.Transactions.Find(id);
             return View(transaction);
         }
 
-        //
-        // GET: /Account/Create
-
+        /// <summary>
+        /// Allows to create a transaction
+        /// </summary>
+        /// <returns>The "Create" view</returns>
         public ActionResult Create()
         {
             return View();
         }
 
-        //
-        // POST: /Account/Create
-
+        /// <summary>
+        /// Allows to create a transaction
+        /// </summary>
+        /// <param name="transaction">The transaction to add</param>
+        /// <returns>The "Index" view if the operation was valid, the "Create" view otherwise</returns>
         [HttpPost]
         public ActionResult Create(TransactionModel transaction)
         {
@@ -91,15 +103,17 @@ namespace Money_Talks.Controllers
 
                 UpdateBalanceAfterAddingTransaction(transaction);
 
-                //return RedirectToAction("runRules", "Rules");
                 return RedirectToAction("Index");
             }
+
             return View(transaction);
         }
 
-        //
-        // GET: /Account/Edit/5
-
+        /// <summary>
+        /// Allows to edit a transaction (serves as a link to another view stage)
+        /// </summary>
+        /// <param name="id">The transaction ID in the database</param>
+        /// <returns>The "Edit" view</returns>
         public ActionResult Edit(int id)
         {
             TransactionModel transaction = transactionsDb.Transactions.Find(id);
@@ -107,9 +121,11 @@ namespace Money_Talks.Controllers
             return View(transaction);
         }
 
-        //
-        // POST: /Account/Edit/5
-
+        /// <summary>
+        /// Allows to edit a transaction
+        /// </summary>
+        /// <param name="transaction">The transaction to be edited</param>
+        /// <returns>The "Index" view if the operation was valid, the "Edit" view otherwise</returns>
         [HttpPost]
         public ActionResult Edit(TransactionModel transaction)
         {
@@ -123,21 +139,26 @@ namespace Money_Talks.Controllers
                 
                 return RedirectToAction("Index");
             }
+
             return View(transaction);
         }
 
-        //
-        // GET: /Account/Delete/5
-
+        /// <summary>
+        /// Allows to delete a transaction from the database
+        /// </summary>
+        /// <param name="id">The record ID to be deleted</param>
+        /// <returns>The "Delete" view</returns>
         public ActionResult Delete(int id)
         {
             TransactionModel transaction = transactionsDb.Transactions.Find(id);
             return View(transaction);
         }
 
-        //
-        // POST: /Account/Delete/5
-
+        /// <summary>
+        /// Handles delete confirmation
+        /// </summary>
+        /// <param name="id">The record ID to be delete</param>
+        /// <returns>The "Index" view</returns>
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
         {
@@ -150,22 +171,38 @@ namespace Money_Talks.Controllers
             return RedirectToAction("Index");
         }
 
+        /// <summary>
+        /// A link to the "pop" tab view
+        /// </summary>
+        /// <returns>The "Pop" view</returns>
         public ActionResult Pop()
         {
             return View();
         }
 
+        /// <summary>
+        /// A link to the "about" tab view
+        /// </summary>
+        /// <returns>The "About" view</returns>
         public ActionResult About()
         {
             return View();
         }
 
+        /// <summary>
+        /// Dispose all managed and unmanaged resources
+        /// </summary>
+        /// <param name="disposing">If true release both managed and unmanaged resources</param>
         protected override void Dispose(bool disposing)
         {
             transactionsDb.Dispose();
             base.Dispose(disposing);
         }
 
+        /// <summary>
+        /// Update the balance attribute once a transaction is added
+        /// </summary>
+        /// <param name="transaction">The transaction to be added</param>
         private void UpdateBalanceAfterAddingTransaction(TransactionModel transaction)
         {
             var user = from u in usersDb.UsersDB
@@ -179,6 +216,10 @@ namespace Money_Talks.Controllers
             usersDb.SaveChanges();
         }
 
+        /// <summary>
+        /// Update the balance attribute once a transaction is removed
+        /// </summary>
+        /// <param name="transaction">The transaction to be removed</param>
         private void UpdateBalanceAfterRemovingTransaction(TransactionModel transaction)
         {
             var user = from u in usersDb.UsersDB
@@ -192,8 +233,12 @@ namespace Money_Talks.Controllers
             usersDb.SaveChanges();
         }
 
-        //---------------------------------------------------------------------
-
+        /// <summary>
+        /// Check balance addition
+        /// </summary>
+        /// <param name="balance">The balance to check</param>
+        /// <param name="transactionAmount">The amount to add</param>
+        /// <returns>The updated balance</returns>
         private int balanceCheckAdd(int balance, int transactionAmount)
         {
             balance += transactionAmount;
@@ -201,6 +246,12 @@ namespace Money_Talks.Controllers
             return balance;
         }
 
+        /// <summary>
+        /// Check balance subtraction
+        /// </summary>
+        /// <param name="balance">The balance to check</param>
+        /// <param name="transactionAmount">The amount to subtract</param>
+        /// <returns>The updated balance</returns>
         private int balanceCheckRemove(int balance, int transactionAmount)
         {
             balance -= transactionAmount;
--- Money_Talks/Models/TransactionModel.cs
+++ Money_Talks/Models/TransactionModel.cs
@@ -5,35 +5,63 @@ using System.Data.Entity;
 
 namespace Money_Talks.Models
 {
-    
+    /// <summary>
+    /// This class represents a user transaction
+    /// </summary>
     public class TransactionModel
     {
+        /*
+         * The transaction ID is a unique value per record.
+         * Serves as the key in the database.
+         */
         [Key]
         public int TransactionId { get; set; }
 
+        /*
+         * Represents the current user-name
+         */
         public string Username { get; set; }
 
+        /*
+         * Represents the amount given by the user
+         */
         [DisplayName("Amount")]
         [Required(ErrorMessage = "Amount is required")]
         [Range(1, Int32.MaxValue, ErrorMessage = "The amount must be bigger than zero!")]
         public long Amount { get; set; }
 
+        /*
+         * Represents the transaction-type
+         * Possible values are income or outcome
+         */
         [DisplayName("Transaction Type")]
         [Required(ErrorMessage = "Transaction type is required")]
         public string TransactionType { get; set; }
 
+        /*
+         * Represents the category of the transaction
+         */
         [DisplayName("Category")]
         [Required(ErrorMessage = "Category is required")]
         public string Category { get; set; }
 
+        /*
+         * Represents the creation date of the transaction
+         */
         [DisplayName("Date Created")]
         [Required(ErrorMessage = "Date is required")]
         [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime DateCreated { get; set; }
     }
 
+    /// <summary>
+    /// Define the database context
+    /// </summary>
     public class AccountDbContext : DbContext
     {
+        /// <summary>
+        /// Define all of the entities in the context
+        /// </summary>
         public DbSet<TransactionModel> Transactions { get; set; }   
     }
 }
